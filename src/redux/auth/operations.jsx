42b696ad9e1388afd661315e9964bbba0e2bb79d
sergeysheva32 <sergeysheva32@gmail.com>
2023-12-23 08:51:19 +0100

    Update operations.jsx

--- src/redux/auth/operations.jsx
+++ src/redux/auth/operations.jsx
@@ -3,13 +3,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
-const setAuthHeader = token => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
+//const setAuthHeader = token => {
+//    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//};
 
-const clearAuthHeader = () => {
-    axios.defaults.headers.common.Authorization = '';
-};
+//const clearAuthHeader = () => {
+//    axios.defaults.headers.common.Authorization = '';
+//};
 
 
 
